d8acef93badc17100c060a430e661ff1da16d3e0
Marvin <marvin.fichtner@yahoo.de>
2024-11-24 17:55:14 +0100

    Second Initial Commit

--- main.cc
+++ main.cc
@@ -1,11 +1,30 @@
+//============================================================================
+// Name        : INF3_Prak.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <string>
 #include <iostream>
 
-int main()
-{
-    for (int i = 0; i <= 5; i++)
-    {
-        std::cout << "Hello world!" << std::endl;
-    }
+#include <cstdio>      /* printf, NULL */
+#include <cstdlib>     /* srand, rand */
+#include <ctime>       /* time */
+
+#include <unistd.h>
+
+#include "TASK3.h"
+
+
+using namespace std;
+
+
+int main(){
+	srand(time(nullptr));
+
+    TASK3::demoTask3();
 
     return 0;
 }
